2705a93a404ab5f5249eb4180f42fae4879aa710
Peter <fpv5gr@virginia.edu>
2022-04-06 17:57:19 -0400

    third commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -19,10 +19,17 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      square: Array(9).fill(null),
+      squares: Array(9).fill(null),
     };
   }
   
+handleClick(i) {
+  const squares =
+  this.state.squares.slice();
+  squares[i] = 'X';
+  this.setState({squares: squares});
+}
+
   renderSquare(i) {
     return (
       <Square
